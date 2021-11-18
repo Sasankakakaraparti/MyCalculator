326c4d75f3e8d18e6f81e8c6d233aa38bd57cb27
sasankasrinivas99@gmail.com <sasankakakaraparti@gmail.com>
2021-11-18 10:33:37 +0530

    created components

--- mycalci/src/app/app.module.ts
+++ mycalci/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { MainBodyComponent } from './MyComponents/main-body/main-body.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainBodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
